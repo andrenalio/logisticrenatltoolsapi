953b159a452b2f72ef5766d0325fc3bd0766730b
André Nalio <andre.bnalio@gmail.com>
2025-10-28 15:11:23 -0300

    Commiting multitenant structure

---
+++ src/test/java/com/praxium/api/logisticrentaltools/LogisticrentaltoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praxium.api.logisticrentaltools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticrentaltoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
